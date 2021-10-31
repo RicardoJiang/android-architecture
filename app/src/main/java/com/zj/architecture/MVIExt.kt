e5ce16463ec6f818560ee4aa3557644cbd2cf274
shenzhen2017 <2868405029@qq.com>
2021-10-31 18:15:55 +0800

    MOD: details

--- app/src/main/java/com/zj/architecture/MVIExt.kt
+++ app/src/main/java/com/zj/architecture/MVIExt.kt
@@ -51,7 +51,7 @@ internal data class StateTuple1<A>(val a: A)
 internal data class StateTuple2<A, B>(val a: A, val b: B)
 internal data class StateTuple3<A, B, C>(val a: A, val b: B, val c: C)
 
-inline fun <reified T> MutableLiveData<T>.setState(reducer: T.() -> T) {
+fun <T> MutableLiveData<T>.setState(reducer: T.() -> T) {
     this.value = this.value?.reducer()
 }
 
