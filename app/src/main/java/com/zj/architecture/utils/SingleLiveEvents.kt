6a170fd06bf27e8d5068fa70b93a3a4f239f1c0d
shenzhen2017 <2868405029@qq.com>
2021-11-09 20:40:13 +0800

    FIX: SingleLiveEvents覆盖问题

--- app/src/main/java/com/zj/architecture/utils/SingleLiveEvents.kt
+++ app/src/main/java/com/zj/architecture/utils/SingleLiveEvents.kt
@@ -14,11 +14,13 @@ import java.util.concurrent.atomic.AtomicBoolean
  */
 class SingleLiveEvents<T> : MutableLiveData<List<T>>() {
     private val pending = AtomicBoolean(false)
+    private val eventList = mutableListOf<List<T>>()
 
     @MainThread
     override fun observe(owner: LifecycleOwner, observer: Observer<in List<T>>) {
         super.observe(owner, { t ->
             if (pending.compareAndSet(true, false)) {
+                eventList.clear()
                 observer.onChanged(t)
             }
         })
@@ -27,6 +29,10 @@ class SingleLiveEvents<T> : MutableLiveData<List<T>>() {
     @MainThread
     override fun setValue(t: List<T>?) {
         pending.set(true)
-        super.setValue(t)
+        t?.let {
+            eventList.add(it)
+        }
+        val list = eventList.flatten()
+        super.setValue(list)
     }
 }
